b40e1279245a700de938d3d129fa12c7c151509c
moinabyssinia <michaelg.tadesse@gmail.com>
2021-01-28 18:07:31 -0500

    contact transition

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
+const contactDiv = document.querySelector("#contact");
+
 scrollingElement = (document.scrollingElement || document.body)
 function scrollToBottom () {
    scrollingElement.scrollTop = scrollingElement.scrollHeight;
+   // change div border color
+   contactDiv.style.backgroundColor = "#e4c8c8";
+   
+   setTimeout(() => {
+      contactDiv.style.backgroundColor = "#ffffff";
+   }, 2000)
 }
 
-//sidebar logic
